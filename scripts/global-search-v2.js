2ea227bdc94627f36646ee49ea6a98ff25129a56
Stephen Thomas <stepheniki17@gmail.com>
2023-04-21 16:07:42 +0200

    global

--- scripts/global-search-v2.js
+++ scripts/global-search-v2.js
@@ -1,66 +1,52 @@
 
 /************************************************************************************************************************************** */
-//                                          Barre de recherche principale  - Algorythme version 2 - Boucles natives "for"
+//                                          Barre de recherche principale  - Algorythme version 2 - Boucles "foreach"
 /************************************************************************************************************************************** */
-
 function globalSearch() {
-    const inputArray = [] // Création d'un tableau vide
-    let searchField = document.getElementById('principal_search-bar').value // Séléction de la barre de recherche principale
-
+    const inputArray = []
+    let searchField = document.getElementById('principal_search-bar').value
     if (searchField.length < 3) {
-        document.querySelector('.search_error').style.display = "block" // SI on tape moins de 3 lettres ... Message erreur s'affiche 
+        document.querySelector('.search_error').style.display = "block" // 
     } else {
-        document.querySelector('.search_error').style.display = "none" // OU on tape plus de 3 lettres ... Message erreur disparait
-
-        for (let i = 0; i < recipes.length; i++) { // Incrémentation de la valeur "letters" // passe sur chacune des recettes
+        document.querySelector('.search_error').style.display = "none"
+        recipes.forEach(recipe => { // Incrémentation de la valeur "letters" 
             let displaySort = false // Affichage du tri
-
-            // Recherche du texte tapé dans les noms de recettes
-            let names = recipes[i].name
-            if (names.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) { // Si les lettres tappées sont trouvées dans le nom de la recette...
-                inputArray.push(recipes[i])
+            let names = recipe.name
+            if (names.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) {
+                inputArray.push(recipe)
                 displaySort = true  // Affichage des résultats triés
-                document.querySelector('.total').innerHTML = inputArray.length + " " + "recettes trouvées" // Afficher nombre de recettes
+                document.querySelector('.total').innerHTML = inputArray.length + " " + "recettes trouvées"
                 document.querySelector('.total').style.display = "block"
-                document.querySelector('.search_error_recipe').style.display = "none"
             }
-
-            // Recherche du texte tapé dans les ingredients des recettes
-            let ingredients = recipes[i].ingredients
-            for (let j = 0; j < ingredients.length; j++) {
-                let ingredientsList = ingredients[j].ingredient // recherche dans ingredients de la liste d'ingredients
-                if (ingredientsList.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) { // Si les lettres tappées sont trouvées dans le nom des ingredients...
-                    inputArray.push(recipes[i])
+            recipe.ingredients.forEach(ingredient => {
+                let ingredientsList = ingredient.ingredient // recherche dans ingredients de la liste d'ingredients
+                if (ingredientsList.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) {
+                    inputArray.push(recipe)
                     displaySort = true // Affichage des résultats triés
                     document.querySelector('.total').innerHTML = inputArray.length + " " + "recettes trouvées" // Afficher nombre de recettes
                     document.querySelector('.total').style.display = "block"
-                    document.querySelector('.search_error_recipe').style.display = "none"
                 }
-            }
-
-            // Recherche du texte tapé dans la description des recettes
-            let description = recipes[i].description
+            })
+            let description = recipe.description
             if (description.toLowerCase().includes(searchField.toLowerCase()) && displaySort == false) { // Si les lettres tappées sont trouvées dans la description...
-                inputArray.push(recipes[i])
+                inputArray.push(recipe)
                 displaySort = true // Affichage des résultats triés
                 document.querySelector('.total').innerHTML = inputArray.length + " " + "recettes trouvées" // Afficher nombre de recettes
                 document.querySelector('.total').style.display = "block"
-                document.querySelector('.search_error_recipe').style.display = "none"
             }
-        }
 
-        // Afficher message erreur quand aucune recette trouvée
-        if (inputArray.length === 0) {
-            document.querySelector('.total').style.display = "none";
-            document.querySelector('.search_error_recipe').style.display = "block";
-            document.querySelector('.search_error_recipe').innerHTML = "Aucune recette trouvée ! Essayez 'tarte aux pommes' ou 'poisson' par exemple...";
-        }
+            // Afficher message erreur quand aucune recette trouvée
+            if (inputArray.length === 0) {
+                document.querySelector('.total').style.display = "none";
+                document.querySelector('.search_error_recipe').style.display = "block";
+                document.querySelector('.search_error_recipe').innerHTML = "Aucune recette trouvée ! Essayez 'tarte aux pommes' ou 'poisson' par exemple...";
+            }
 
-        let inputUniqueArray = [...new Set(inputArray)]; // Afficher tableau sans les doublons
-
-        displayRecipe(inputUniqueArray) // Affichage des recettes dans ce nouveau tableau
-        displayLists(inputUniqueArray)
-        tagsListener()
+            let inputUniqueArray = [...new Set(inputArray)]; // Afficher tableau sans les doublons
+            displayRecipe(inputUniqueArray) // Affichage des recettes dans ce nouveau tableau
+            displayLists(inputUniqueArray)
+            tagsListener()
+        })
     }
 }
 
@@ -68,25 +54,20 @@ function globalSearch() {
 const searchInput = document.getElementById('principal_search-bar');
 
 searchInput.addEventListener('input', function () {
-
-
     if (searchInput.value.length > 2) { // Si au moins 3 lettres tapées, le tri se fait...
         globalSearch()
 
     } else if (searchInput.value.length == 0) {
         document.querySelector('.search_error').style.display = "none"
         document.querySelector('.total').style.display = "none"
-        document.querySelector('.search_error_recipe').style.display = "none"
+
 
     } else { // ... sinon message d'erreur
         displayRecipe(recipes)
         document.querySelector('.search_error').style.display = "block"
-        document.querySelector('.search_error_recipe').style.display = "none"
-
     }
+
 
 })
 
 document.querySelector('.total').style.display = "none"
-
-
